aad7b56b8f7c08acd61ae3b0ad187e9c832c648b
rohanvidhan <rohan.vidhan@gmail.com>
2023-01-10 23:58:34 -0600

    Adding Routing

---
+++ src/components/routes/navigation/navigation.component.jsx
@@ -0,0 +1,23 @@
+import "./navigation.styles.scss";
+import { ReactComponent as CrwnLogo } from "../../../assets/crown.svg";
+import { Link, Outlet } from "react-router-dom";
+import { Fragment } from "react";
+const Navigation = ()=>{
+  return (
+   <Fragment>
+      <div className="navigation">
+        <Link className="logo-container" to="/">
+            <CrwnLogo></CrwnLogo>
+        </Link>        
+        <div className="nav-links-container">
+            <Link className="nav-link" to="/shop">
+                SHOP
+            </Link>
+        </div>
+      </div>
+      <Outlet></Outlet>
+    </Fragment>
+   
+  );
+};
+export default Navigation;
